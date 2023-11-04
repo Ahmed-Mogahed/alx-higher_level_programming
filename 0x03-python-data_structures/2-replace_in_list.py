67180ab9d45b7b58805708562e1e3632f4409919
Ahmed-Mogahed <amogahed91@gmail.com>
2023-11-04 10:52:57 -0700

    0x03-python-data_structures

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
+
 def replace_in_list(my_list, idx, element):
-    if idx >= 0 and idx < len(my_list):
-        my_list.insert(idx, element)
+    if my_list is not None and -1 < idx < len(my_list):
+        my_list[idx] = element
     return my_list
